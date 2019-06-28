1a5485d0edffc2e8fdf71626cfc8a588615fcd45
Kinuthia <kinuthiaduncan@gmail.com>
2019-06-28 15:46:28 +0300

    shutdowns

--- src/views/apps/Censorship.vue
+++ src/views/apps/Censorship.vue
@@ -35,12 +35,28 @@
                 </div>
             </el-col>
         </el-row>
+
+        <el-row class="mt-30" :gutter="30">
+            <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12" style="padding: 0!important;">
+                <div class="card-base card-shadow--medium p-20" style="height:450px" v-loading="!asyncChart1">
+                    <div id="geo-map" style="height:450px; width:100%"></div>
+                </div>
+            </el-col>
+            <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12" style="padding: 0!important;">
+                <div class="card-base card-shadow--medium p-20" style="height:450px" v-loading="!asyncChart1">
+
+                </div>
+            </el-col>
+        </el-row>
+
     </vue-scroll>
 </template>
 
 <script>
     import moment from 'moment';
-    import LineChart from '../pages/LineChart.vue'
+    import LineChart from '../pages/LineChart.vue';
+    import * as echarts from 'echarts';
+    import $ from 'jquery';
     export default {
         name: "censorship",
         components: {
@@ -58,6 +74,8 @@
                 google_trends_url: "censorship/google_trends",
                 google_trends_data: {},
                 chartTitle: 'defaultValue',
+                all_shutdowns_url: 'censorship/shutdowns/all',
+                allShutdowns: []
             }
         },
         computed: {
@@ -112,6 +130,73 @@
                     this.graph.xAxis.data.push(new Date(date).toLocaleDateString());
                 }
             },
+            plotMap: function() {
+                this.$http.get(this.all_shutdowns_url).then(response => {
+                    this.allShutdowns = response.data.data;
+                    let dataHolder = [];
+                    for(let item in this.allShutdowns){
+                        dataHolder.push({name: item, value: this.allShutdowns[item]});
+                    }
+                    let myChart;
+                    myChart = echarts.init(document.getElementById('geo-map'));
+                    myChart.showLoading();
+                    $.get('custom.geo.json', eastenAfrica => {
+                        myChart.hideLoading();
+                        echarts.registerMap('EA', eastenAfrica);
+                        let option = {
+                            title: {
+                                text: 'Internet shutdowns Eastern Africa',
+                                subtext: 'Data from www.accessnow.org',
+                                left: 'right'
+                            },
+                            tooltip: {
+                                trigger: 'item',
+                                showDelay: 0,
+                                transitionDuration: 0.2,
+                                formatter: function (params) {
+                                    var value = (params.value + '').split('.');
+                                    value = value[0].replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
+                                    return params.seriesName + '<br/>' + params.name + ': ' + value;
+                                }
+                            },
+                            visualMap: {
+                                left: 'right',
+                                min: 0,
+                                max: 15,
+                                inRange: {
+                                    color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
+                                },
+                                text:['High','Low'],
+                                calculable: true
+                            },
+                            toolbox: {
+                                show: true,
+                                left: 'left',
+                                top: 'top',
+                                feature: {
+                                    dataView: {readOnly: false},
+                                    restore: {}
+                                }
+                            },
+                            series: [
+                                {
+                                    name: 'Internet shutdowns',
+                                    type: 'map',
+                                    roam: true,
+                                    map: 'EA',
+                                    itemStyle:{
+                                        emphasis:{label:{show:true}}
+                                    },
+                                    data: dataHolder
+                                }
+                            ]
+                        };
+                        myChart.setOption(option);
+                    });
+                }).catch(error => {
+                    console.log(error);
+                });
+            },
             __resizeHanlder: _.throttle(function (e) {
                 if(this.resized) {
                     this.asyncComponent = null;
@@ -119,6 +204,9 @@
                 }
                 this.resized = true
             }, 700),
+        },
+        mounted: function () {
+            this.plotMap();
         }
     }
 </script>
